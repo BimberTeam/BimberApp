cba2a16bf806e19a80d6f3bdc4951888e81f9a0f
jmysliv <jjmreal34@gmail.com>
2020-08-09 19:58:41 +0200

    Added silver app bar

--- bimber/lib/ui/details/user_details.dart
+++ bimber/lib/ui/details/user_details.dart
@@ -2,13 +2,133 @@ import 'package:bimber/models/user.dart';
 import 'package:flutter/material.dart';
 
 class UserDetails extends StatelessWidget{
+  final User user;
+  final Function like;
+  final Function dislike;
+  UserDetails({
+  @required this.user,
+  this.like,
+  this.dislike});
 
-  
+  ClipOval createButton(IconData icon, Color iconColor, Function onTap, BuildContext context){
+    return  ClipOval(
+      child: Material(
+        color: Theme.of(context).colorScheme.primaryVariant, // button color
+        child: InkWell(
+          splashColor: Theme.of(context).accentColor, // inkwell color
+          child: Icon(icon, color: iconColor, size: 50),
+          onTap: onTap,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    double _appBarHeight = MediaQuery.of(context).size.height*0.7;
     return Scaffold(
       body: Container(
-        child: Text("user detail screen"),
+        child: Stack(
+          alignment: AlignmentDirectional.bottomCenter,
+          children: <Widget>[
+            CustomScrollView(
+              shrinkWrap: false,
+              slivers: <Widget>[
+                SliverAppBar(
+                  elevation: 0.0,
+                  forceElevated: true,
+                  leading: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: Icon(
+                      Icons.arrow_back,
+                      size: 30.0,
+                    ),
+                  ),
+                  expandedHeight: _appBarHeight,
+                  pinned: true,
+                  floating: true,
+                  snap: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                    title: Text(user.name),
+                    background: Stack(
+                      fit: StackFit.expand,
+                      children: <Widget>[
+                        Positioned(
+                          child: Image.network(user.imagePath, height: _appBarHeight, fit: BoxFit.cover,)
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SliverList(
+                  delegate: SliverChildListDelegate(<Widget>[
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(35.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.only(bottom: 20.0),
+                              alignment: Alignment.center,
+                              child: Row(
+                                mainAxisAlignment:
+                                MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+//                                  Row(
+//                                    children: <Widget>[
+//                                      Icon(
+//                                        Icons.access_time,
+//                                        color: Colors.cyan,
+//                                      ),
+//                                      Padding(
+//                                        padding:
+//                                        const EdgeInsets.all(8.0),
+//                                        child: new Text("10:00  AM"),
+//                                      )
+//                                    ],
+//                                  ),
+//                                  Row(
+//                                    children: <Widget>[
+//                                      Icon(
+//                                        Icons.map,
+//                                        color: Colors.cyan,
+//                                      ),
+//                                      Padding(
+//                                        padding:
+//                                        const EdgeInsets.all(8.0),
+//                                        child: Text("15 MILES"),
+//                                      )
+//                                    ],
+//                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 500,),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ]),
+                ),
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              height: 100,
+              child: Row(
+                mainAxisAlignment:
+                MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  createButton(Icons.clear, Colors.red, dislike, context),
+                  createButton(Icons.check, Colors.green, like, context)
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- bimber/lib/ui/home/home_screen.dart
+++ bimber/lib/ui/home/home_screen.dart
@@ -1,9 +1,27 @@
+import 'package:bimber/models/age_preference.dart';
+import 'package:bimber/models/alcohol.dart';
+import 'package:bimber/models/alcohol_type.dart';
+import 'package:bimber/models/gender.dart';
+import 'package:bimber/models/location.dart';
+import 'package:bimber/models/user.dart';
 import 'package:bimber/ui/details/user_details.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return UserDetails();
+    return UserDetails(user: User(id: null,
+        name: "Harnas",
+        email: null,
+        gender: Gender.Male,
+        age: 6,
+        description: "Harnas, piwo z gór",
+        favoriteAlcohol: Alcohol(name: "Harnas", type: AlcoholType.Beer),
+        genderPreference: Gender.Male,
+        agePreference: AgePreference(from: 18, to: 99),
+        alcoholPreference: AlcoholType.Beer,
+        imagePath: "https://upload.wikimedia.org/wikipedia/commons/8/85/Harna%C5%9B_glass_bottle.png",
+        location: Location(latitude: 50.44, longtitude: 56.78),
+        friends: null));
   }
 }
