5498dd6e1b8797597a0d17f0c797e9ef0f306f4d
jmysliv <jjmreal34@gmail.com>
2020-10-11 22:34:56 +0200

    Added animated list for current member

--- bimber/lib/ui/group_create/group_create_draggable_list.dart
+++ bimber/lib/ui/group_create/group_create_draggable_list.dart
@@ -1,4 +1,5 @@
 import 'package:bimber/models/user.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:build_context/build_context.dart';
 import 'package:bimber/ui/group_details/user_image_hero.dart';
@@ -17,6 +18,8 @@ class _GroupCreateDraggableListState extends State<GroupCreateDraggableList> {
   List<User> friendsAdded;
   User userDragged;
   final GlobalKey<AnimatedListState> listKey = GlobalKey<AnimatedListState>();
+  final GlobalKey<AnimatedListState> membersListKey =
+      GlobalKey<AnimatedListState>();
 
   @override
   void initState() {
@@ -27,109 +30,121 @@ class _GroupCreateDraggableListState extends State<GroupCreateDraggableList> {
 
   Widget sizeIt(BuildContext context, User user, int index, animation) {
     return SizeTransition(
-      axis: Axis.horizontal,
-      sizeFactor: animation,
-      child: SizedBox(
-        width: 100,
+        axis: Axis.horizontal,
+        sizeFactor: animation,
         child: LongPressDraggable(
-          child: _animatedListTile(user, context),
-          childWhenDragging: _animatedListTile(user, context),
-          feedback: _animatedListTile(user, context),
+          child: _draggableChild(user, context),
+          childWhenDragging: _draggableChild(user, context),
+          feedback: _draggableChild(user, context),
           onDragStarted: () {
             userDragged = user;
-            listKey.currentState.removeItem(
-                index, (_, animation) => sizeIt(context, user, index, animation),
+            listKey.currentState.removeItem(index,
+                (_, animation) => sizeIt(context, user, index, animation),
                 duration: Duration(milliseconds: 500));
             friendsAvailable.removeAt(index);
           },
-          onDraggableCanceled: (Velocity velocity, Offset offset){
+          onDraggableCanceled: (Velocity velocity, Offset offset) {
             friendsAvailable.insert(index, userDragged);
-            listKey.currentState.insertItem(index,
-                duration: Duration(milliseconds: 500));
+            listKey.currentState
+                .insertItem(index, duration: Duration(milliseconds: 500));
           },
-        )
-      ),
-    );
+          onDragCompleted: () {
+            friendsAdded.insert(0, userDragged);
+            membersListKey.currentState
+                .insertItem(0, duration: Duration(milliseconds: 500));
+          },
+        ));
   }
 
-  Widget _animatedListTile(User user, BuildContext context) {
+  Widget _draggableChild(User user, BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(5),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Theme.of(context).colorScheme.primaryVariant,
-          boxShadow: [
-            BoxShadow(
-                blurRadius: 3,
-                offset: Offset(3, 3),
-                color: Colors.black.withOpacity(0.4))
-          ]),
-      child: Row(
+        margin: EdgeInsets.all(5),
+        padding: EdgeInsets.only(top: 5),
+        width: 90,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Theme.of(context).colorScheme.primaryVariant,
+            boxShadow: [
+              BoxShadow(
+                  blurRadius: 3,
+                  offset: Offset(3, 3),
+                  color: Colors.black.withOpacity(0.4))
+            ]),
+        child: Column(
           children: [
-          Container(
-              padding: EdgeInsets.only(top: 5),
-              width: 90,
-              child: Column(
-                children: [
-                  UserImageHero(
-                      radius: BorderRadius.circular(15.0),
-                      user: user,
-                      size: Size(60, 60),
-                      onTap: () {}),
-                  Text(
-                    user.name,
-                    style: TextStyle(
-                        color: Theme.of(context).colorScheme.secondaryVariant,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w900,
-                        fontFamily: 'Baloo',
-                        decoration: TextDecoration.none),
-                  )
-                ],
-              )
-          ),
-            Container(
-              child: Column(
-                children: [
-                  AnimatedContainer(
-                    width: 0,
-                    height: 0,
-                    duration: Duration(milliseconds: 500),
-                    child: Text(
-                      user.name,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                      style: TextStyle(
-                          color: Theme.of(context).colorScheme.secondaryVariant,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w900,
-                          fontFamily: 'Baloo'),
-                    ),
-                  ),
-                  AnimatedContainer(
-                    width: 0,
-                    height: 0,
-                    duration: Duration(milliseconds: 500),
-                    child: Text(
-                      user.age.toString(),
-                      style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w900,
-                          fontFamily: 'Baloo'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            AnimatedContainer(
-              width: 0,
-              height: 0,
-              duration: Duration(milliseconds: 500),
-              child: Icon(Icons.delete, color: Colors.transparent, size: 15),
+            UserImageHero(
+                radius: BorderRadius.circular(15.0),
+                user: user,
+                size: Size(60, 60),
+                onTap: () {}),
+            Text(
+              user.name,
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondaryVariant,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900,
+                  fontFamily: 'Baloo',
+                  decoration: TextDecoration.none),
             )
-          ]
-      ),
+          ],
+        ));
+  }
+
+  Widget _animatedListTile(
+      BuildContext context, User user, int index, Animation<double> animation) {
+    return SizeTransition(
+      sizeFactor: animation,
+      child: Container(
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Theme.of(context).colorScheme.primaryVariant,
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 3,
+                    offset: Offset(3, 3),
+                    color: Colors.black.withOpacity(0.4))
+              ]),
+          child: ListTile(
+              leading: UserImageHero(
+                  radius: BorderRadius.circular(15.0),
+                  user: user,
+                  size: Size(60, 60),
+                  onTap: () {}),
+              title: Text(
+                user.name,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.secondaryVariant,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w900,
+                    fontFamily: 'Baloo'),
+              ),
+              subtitle: Text(
+                user.age.toString(),
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w900,
+                    fontFamily: 'Baloo'),
+              ),
+              trailing: GestureDetector(
+                onTap: () {
+                  friendsAvailable.insert(0, user);
+                  listKey.currentState
+                      .insertItem(0, duration: Duration(milliseconds: 500));
+                  membersListKey.currentState.removeItem(
+                      index,
+                      (_, animation) =>
+                          _animatedListTile(context, user, index, animation),
+                      duration: Duration(milliseconds: 500));
+                  setState(() {
+                    friendsAdded.removeAt(index);
+                  });
+                },
+                child: Icon(Icons.delete, color: Colors.red, size: 25),
+              ))),
     );
   }
 
@@ -148,8 +163,9 @@ class _GroupCreateDraggableListState extends State<GroupCreateDraggableList> {
                       initialItemCount: friendsAvailable.length,
                       key: listKey,
                       scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index, animation) => sizeIt(context, friendsAvailable[index], index, animation),
-                )),
+                      itemBuilder: (context, index, animation) => sizeIt(
+                          context, friendsAvailable[index], index, animation),
+                    )),
                 Text("Wybierz członków",
                     style: TextStyle(
                         color: Theme.of(context).colorScheme.primaryVariant,
@@ -160,13 +176,63 @@ class _GroupCreateDraggableListState extends State<GroupCreateDraggableList> {
             )),
         Expanded(
           child: Container(
-              decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.primary,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(50.0),
-                    topRight: Radius.circular(50.0),
-                  )),
-              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),),
+            decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50.0),
+                  topRight: Radius.circular(50.0),
+                )),
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+            child: DragTarget(
+              onWillAccept: (data) => true,
+              builder: (BuildContext context, candidateData, rejectedData) {
+                Color borderColor;
+                if (candidateData.isEmpty)
+                  borderColor = Colors.grey;
+                else
+                  borderColor = Colors.green;
+                return DottedBorder(
+                  radius: Radius.circular(15),
+                  borderType: BorderType.RRect,
+                  color: borderColor,
+                  strokeWidth: 2,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width - 40,
+                    child: Stack(
+                      children: [
+                        Container(
+                            alignment: Alignment.center,
+                            child: Text(
+                              "Przytrzymaj i przeciągnij znajomych",
+                              style: TextStyle(
+                                color: friendsAdded.isEmpty
+                                    ? Theme.of(context)
+                                        .colorScheme
+                                        .secondaryVariant
+                                    : Colors.transparent,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w900,
+                                fontFamily: 'Baloo',
+                              ),
+                              textAlign: TextAlign.center,
+                            )),
+                        Positioned.fill(
+                            child: AnimatedList(
+                          initialItemCount: friendsAdded.length,
+                          key: membersListKey,
+                          scrollDirection: Axis.vertical,
+                          itemBuilder:
+                              (context, index, Animation<double> animation) =>
+                                  _animatedListTile(context,
+                                      friendsAdded[index], index, animation),
+                        )),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
         )
       ],
     );
--- bimber/lib/ui/group_create/group_create_screen.dart
+++ bimber/lib/ui/group_create/group_create_screen.dart
@@ -3,23 +3,21 @@ import 'package:bimber/ui/group_create/group_create_draggable_list.dart';
 import 'package:flutter/material.dart';
 
 class GroupCreateScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.secondary,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        centerTitle: true,
-        title: Text("Stwórz grupę",
-            style: TextStyle(
-                color: Theme.of(context).colorScheme.primaryVariant,
-                fontSize: 20,
-                fontWeight: FontWeight.w900,
-                fontFamily: 'Baloo')),
-      ),
-      body: GroupCreateDraggableList(friends: Fixtures.getUsersList())
-    );
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          centerTitle: true,
+          title: Text("Stwórz grupę",
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.primaryVariant,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900,
+                  fontFamily: 'Baloo')),
+        ),
+        body: GroupCreateDraggableList(friends: Fixtures.getUsersList()));
   }
 }
